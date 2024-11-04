c7e0c080dbed81b1c140c3deb7f5ba15a89dfeca
ClaudineiMacedo <158119994+ClaudineiMacedo@users.noreply.github.com>
2024-11-03 21:36:53 -0300

    Add files via upload

---
+++ aula35.py
@@ -0,0 +1,13 @@
+'''
+Repetições 
+while (enquanto)
+Executar uma ação enquanto uma condição for verdadeira
+Loop infinito -> quando um código não tem fim
+'''
+contador = 0
+
+while contador < 10:
+    contador = contador + 1
+    print(contador)
+
+print('Acabou')
